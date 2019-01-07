6169a171555add4e37037bb72c92c747a0155ef8
Carlos Domínguez García <carlosdg97@gmail.com>
2019-01-07 23:27:26 +0000

    Updates

---
+++ precache-manifest.df5634b7a6bf4ab547381bc53c3c2488.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "19975bb9b5842ad1ae3e",
+    "url": "/StyleTransfer/static/js/runtime~main.19975bb9.js"
+  },
+  {
+    "revision": "ee1cecab000a119c44ee",
+    "url": "/StyleTransfer/static/js/main.ee1cecab.chunk.js"
+  },
+  {
+    "revision": "da7d212670c7dff91b55",
+    "url": "/StyleTransfer/static/js/1.da7d2126.chunk.js"
+  },
+  {
+    "revision": "ee1cecab000a119c44ee",
+    "url": "/StyleTransfer/static/css/main.f9c2cf0b.chunk.css"
+  },
+  {
+    "revision": "32691dc8b0cffa31bb335ed648a291f5",
+    "url": "/StyleTransfer/index.html"
+  }
+];
