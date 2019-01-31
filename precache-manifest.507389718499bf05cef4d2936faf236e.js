7b5f4992012d58be9cb851b66cca07f9ea795ba9
Daute Rodríguez Rodríguez <alu0100973914@ull.edu.es>
2019-01-31 20:27:47 +0000

    Updates

---
+++ precache-manifest.507389718499bf05cef4d2936faf236e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "771e1a7ca65d87746f0d27377dfbe00d",
+    "url": "/StyleTransfer/static/media/undraw_image_upload_wqh3.771e1a7c.svg"
+  },
+  {
+    "revision": "a3e1b919857dc0c7d6f0",
+    "url": "/StyleTransfer/static/css/main.df4bb1b5.chunk.css"
+  },
+  {
+    "revision": "0a08469fe3e16f5751a6",
+    "url": "/StyleTransfer/static/js/1.0a08469f.chunk.js"
+  },
+  {
+    "revision": "19975bb9b5842ad1ae3e",
+    "url": "/StyleTransfer/static/js/runtime~main.19975bb9.js"
+  },
+  {
+    "revision": "7a6edf59c43513817e373ea7ffe04230",
+    "url": "/StyleTransfer/static/media/undraw_making_art_759c.7a6edf59.svg"
+  },
+  {
+    "revision": "a3e1b919857dc0c7d6f0",
+    "url": "/StyleTransfer/static/js/main.a3e1b919.chunk.js"
+  },
+  {
+    "revision": "c0bcd0b87350e6521ed45702a596c0ce",
+    "url": "/StyleTransfer/static/media/undraw_safe_bnk7.c0bcd0b8.svg"
+  },
+  {
+    "revision": "f756d44b030d7876eb388a656df2fbe3",
+    "url": "/StyleTransfer/static/media/carlos.f756d44b.png"
+  },
+  {
+    "revision": "eb1ecf919f5a906a362937b0b44e57ca",
+    "url": "/StyleTransfer/static/media/daute.eb1ecf91.png"
+  },
+  {
+    "revision": "dc53d9d5d741ffd25907a17f259205f8",
+    "url": "/StyleTransfer/static/media/header-bg.dc53d9d5.jpg"
+  },
+  {
+    "revision": "de36b586e55384a943930a7f5d8c5364",
+    "url": "/StyleTransfer/index.html"
+  }
+];
